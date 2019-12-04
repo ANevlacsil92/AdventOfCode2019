71b9286e4d61541a22094a2eab54cbd05d3cfba1
Andreas Nevlacsil <a.nevlacsil@hotmail.com>
2019-12-04 10:22:11 +0100

    Day3 and Day4

--- AdventOfConsole/Days/Day3.cs
+++ AdventOfConsole/Days/Day3.cs
@@ -7,11 +7,16 @@ namespace AdventOfConsole.Days
 {
     public static class Day3
     {
-        static List<List<String>> positions = new List<List<String>>();
-        static string firstIntersection;
+        static List<List<String>> positions;
+        static List<String> intersections;
+        static List<int> results;
         internal static void christmassySolvePuzzleOne()
         {
-            int counter;
+            positions = new List<List<string>>();
+            intersections = new List<string>();
+            results = new List<int>();
+
+            int counter = 0;
 
             foreach (string path in MainWindow.input.Text.Split("\r\n"))
             {
@@ -57,73 +62,67 @@ namespace AdventOfConsole.Days
 
             var equals = positions[0].Intersect(positions[1]);
 
-
-            String tmpVal;
-            int tmpSum = -1, currSum;
-            bool hasFirst = false;
             foreach (String s in equals)
             {
-                if (!hasFirst)
-                {
-                    hasFirst = true;
-                    firstIntersection = s;
-                }
-                currSum = Math.Abs(int.Parse(s.Split(":")[0])) + Math.Abs(int.Parse(s.Split(":")[1]));
-                if (tmpSum == -1 || tmpSum > currSum)
-                {
-                    tmpSum = currSum;
-                    tmpVal = s;
-                }
+                intersections.Add(s);
+                results.Add(Math.Abs(int.Parse(s.Split(":")[0])) + Math.Abs(int.Parse(s.Split(":")[1])));
             }
+
+            MainWindow.answerOne.Text = results.Min().ToString();
         }
 
         internal static void christmassySolvePuzzleTwo()
         {
-
-            int counter;
-            foreach (string path in MainWindow.input.Text.Split("\r\n"))
+            results.Clear();
+            int counter = 0;
+            foreach (string inters in intersections)
             {
-                int currentX = 0;
-                int currentY = 0;
-                List<String> position = new List<string>();
-                foreach (string s in path.Split(","))
+                int steps = 0;
+                foreach (string path in MainWindow.input.Text.Split("\r\n"))
                 {
-                    switch (s.Substring(0, 1))
+                    int currentX = 0;
+                    int currentY = 0;
+                    foreach (string s in path.Split(","))
                     {
-                        case "R":
-                            for (counter = 1; counter <= int.Parse(s.Substring(1)); counter++)
-                            {
-                                currentX++;
-                                position.Add(currentX + ":" + currentY);
-                            }
-                            break;
-                        case "D":
-                            for (counter = 1; counter <= int.Parse(s.Substring(1)); counter++)
-                            {
-                                currentY++;
-                                position.Add(currentX + ":" + currentY);
-                            }
-                            break;
-                        case "U":
-                            for (counter = 1; counter <= int.Parse(s.Substring(1)); counter++)
-                            {
-                                currentY--;
-                                position.Add(currentX + ":" + currentY);
-                            }
-                            break;
-                        case "L":
-                            for (counter = 1; counter <= int.Parse(s.Substring(1)); counter++)
-                            {
-                                currentX--;
-                                position.Add(currentX + ":" + currentY);
-                            }
-                            break;
+                        switch (s.Substring(0, 1))
+                        {
+                            case "R":
+                                for (counter = 0; counter < int.Parse(s.Substring(1)); counter++)
+                                {
+                                    currentX++;
+                                    if (currentX + ":" + currentY == inters) break;
+                                }
+                                break;
+                            case "D":
+                                for (counter = 0; counter < int.Parse(s.Substring(1)); counter++)
+                                {
+                                    currentY++;
+                                    if (currentX + ":" + currentY == inters) break;
+                                }
+                                break;
+                            case "U":
+                                for (counter = 0; counter < int.Parse(s.Substring(1)); counter++)
+                                {
+                                    currentY--;
+                                    if (currentX + ":" + currentY == inters) break;
+                                }
+                                break;
+                            case "L":
+                                for (counter = 0; counter < int.Parse(s.Substring(1)); counter++)
+                                {
+                                    currentX--;
+                                    if (currentX + ":" + currentY == inters) break;
+                                }
+                                break;
+                        }
+                        steps += counter;
+                        if (currentX + ":" + currentY == inters) break;
                     }
                 }
-                positions.Add(position);
+                results.Add(steps);
             }
 
-            MainWindow.answerOne.Text = count.ToString();
+            MainWindow.answerTwo.Text = results.Min().ToString();
         }
     }
 }
---
+++ AdventOfConsole/Days/Day4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfConsole.Days
+{
+    public static class Day4
+    {
+        static List<string> numbers = new List<string>();
+        internal static void christmassySolvePuzzleOne()
+        {
+            for(int i = int.Parse(MainWindow.input.Text.Split("-")[0]); i <= int.Parse(MainWindow.input.Text.Split("-")[1]); i++)
+            {
+                bool hasMulti = false;
+                bool failsCriteria = false;
+                int last = 0;
+
+                foreach(int d in i.ToString().Select(digit => int.Parse(digit.ToString())))
+                {
+                    if (d == last) hasMulti = true;
+                    if (d < last) failsCriteria = true;
+                    last = d;
+                }
+
+                if (hasMulti && !failsCriteria) numbers.Add(i.ToString());
+
+            }
+
+            MainWindow.answerOne.Text = numbers.Count.ToString();
+        }
+
+        internal static void christmassySolvePuzzleTwo()
+        {
+            int counter = 0;
+            foreach(string s in numbers)
+            {
+                bool hasDouble = false;
+                for(int i = 1; i<=9; i++)
+                {
+                    if (s.Contains(i.ToString() + i.ToString()) && !(s.Contains(i.ToString() + i.ToString() + i.ToString()))) hasDouble = true;
+                }
+                if (hasDouble) counter++;
+            }
+            MainWindow.answerTwo.Text = counter.ToString();
+        }
+    }
+}
